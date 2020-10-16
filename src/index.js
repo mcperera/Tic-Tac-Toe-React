af94b26b2d7b939e41eed9138e0c1a3a48d70d3c
Madushan Perera <madushanperera3817@gmail.com>
2020-10-16 17:15:25 +0530

    Fix ignoring clicks if It has a winner

--- src/index.js
+++ src/index.js
@@ -36,14 +36,17 @@ class Board extends Component{
         this.handleClick = this.handleClick.bind(this);
     }
 
-    handleClick(i){
+    handleClick(i) {
         const squares = this.state.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+          return;
+        }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
-            squares : squares,
-            xIsNext : !this.state.xIsNext,
+          squares: squares,
+          xIsNext: !this.state.xIsNext,
         });
-    }
+      }
 
     renderSquare(i){
         return (
@@ -55,15 +58,14 @@ class Board extends Component{
     }
 
     render(){
-        let status = 'Next Player: ' + (this.state.xIsNext ? 'X' : 'O');
         //const status = this.state.xIsNext ? 'X' : 'O';
 
         const winner = calculateWinner(this.state.squares);
-
-        if(winner){
-            status = 'Winner' + winner;
-        }else{
-            status = 'Next Player: ' + (this.state.xIsNext ? 'X' : 'O');
+        let status;
+        if (winner) {
+          status = 'Winner: ' + winner;
+        } else {
+          status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
         }
 
         return(
@@ -100,24 +102,24 @@ function Game(){
     );
 }
 
-function calculateWinner(squares){
+function calculateWinner(squares) {
     const lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
     ];
-    for (let i; i < lines.length; i++){
-        const [a,b,c] = lines[i];
-        if(squares[a] && squares[a] === squares[b] && squares[a] && squares[c] === squares[c]){
-            return squares[a];
-        }
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
     }
     return null;
-}
+  }
 
 ReactDom.render(<Game/>, document.getElementById('root'));
