cda3346405afe8c003508bdc6b68a26d5bbff1a1
Madushan Perera <madushanperera3817@gmail.com>
2020-10-16 17:06:12 +0530

    cal winner

--- src/index.js
+++ src/index.js
@@ -55,8 +55,16 @@ class Board extends Component{
     }
 
     render(){
-        const status = 'Next Player: ' + (this.state.xIsNext ? 'X' : 'O');
+        let status = 'Next Player: ' + (this.state.xIsNext ? 'X' : 'O');
         //const status = this.state.xIsNext ? 'X' : 'O';
+
+        const winner = calculateWinner(this.state.squares);
+
+        if(winner){
+            status = 'Winner' + winner;
+        }else{
+            status = 'Next Player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
 
         return(
             <div>
@@ -90,6 +98,26 @@ function Game(){
             <div className="game-info"></div>
         </div>
     );
+}
+
+function calculateWinner(squares){
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i; i < lines.length; i++){
+        const [a,b,c] = lines[i];
+        if(squares[a] && squares[a] === squares[b] && squares[a] && squares[c] === squares[c]){
+            return squares[a];
+        }
+    }
+    return null;
 }
 
 ReactDom.render(<Game/>, document.getElementById('root'));
